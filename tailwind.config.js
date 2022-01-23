2be7acda142926656dd33e4c0e40eaa0717a2d32
Taiga Ebihara <>
2022-01-24 01:41:12 +0900

    Create: default layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        base: '#4B636E',
+        primary: '#4BACB8',
+      },
+      spacing: {
+        '400px': '400px'
+      }
+    }
+  },
   variants: {
     extend: {},
   },
