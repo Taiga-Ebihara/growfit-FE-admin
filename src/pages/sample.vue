42ca4b94a4118834b89828a3a41f9c054d6243e0
Taiga Ebihara <>
2022-01-23 23:41:31 +0900

    Fix: lint

--- src/pages/sample.vue
+++ src/pages/sample.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="p-6">
     <div class="flex">
-      <input v-model="newMessage" class="py-1 px-2 rounded border-2" type="text">
-      <button class="ml-1 py-1 px-2 bg-gray-200 rounded border-2" @click="onClickAddButton">add</button>
+      <input
+        v-model="newMessage"
+        class="py-1 px-2 rounded border-2"
+        type="text"
+      />
+      <button
+        class="ml-1 py-1 px-2 bg-gray-200 rounded border-2"
+        @click="onClickAddButton"
+      >
+        add
+      </button>
     </div>
     <div class="mt-4">
-      <p v-for="(message, index) in messageList" :key="index" class="mt-1 text-gray-400">{{ message }}</p>
+      <p
+        v-for="(message, index) in messageList"
+        :key="index"
+        class="mt-1 text-gray-400"
+      >
+        {{ message }}
+      </p>
       <p v-if="!messageList.length" class="mt-1 text-gray-400">no messages</p>
     </div>
   </div>
