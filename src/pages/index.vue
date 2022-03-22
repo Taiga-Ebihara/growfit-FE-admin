b6127720923f09cea342fa678e233296a6f85b71
Taiga Ebihara <>
2022-03-22 20:05:40 +0900

    Fix: redirect

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <div></div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class extends Vue {
+  beforeCreate() {
+    this.$router.push('/news')
+  }
+}
+</script>
