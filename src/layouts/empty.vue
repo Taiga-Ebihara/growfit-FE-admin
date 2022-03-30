f81a836c40b1dd9634ef5724ce80486fc18538b4
Taiga Ebihara <>
2022-03-30 21:23:12 +0900

    Create: error handling

---
+++ src/layouts/empty.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- error pageはpagesとして処理されるため、defaultレイアウトの中身として描画されるが、
+  エラーページはdefaultレイアウトではないレイアウトで表示させたいため、
+  空のカスタムレイアウトを用意してそこの中に表示させる。 -->
+  <div><Nuxt></Nuxt></div>
+</template>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,6 +3,7 @@
     <span class="flex justify-center text-primary text-xl">Login Form</span>
     <BaseTextField v-model="email" class="mt-8" placeholder="メールアドレス"></BaseTextField>
     <BaseTextField v-model="password" class="mt-6" placeholder="パスワード"></BaseTextField>
+    <span v-if="isError" class="flex justify-center items-center mt-6 text-error">ユーザー名またはパスワードが違います</span>
     <div class="flex justify-center items-center">
       <BaseButton class="mt-8" color="primary" style="width: 200px" @click="onClickLogin">Login</BaseButton>
     </div>
@@ -17,6 +18,7 @@ import { Vue, Component } from 'nuxt-property-decorator'
 })
 export default class extends Vue {
   LoginRepository = this.$repositories.login
+  isError: boolean = false
   email: string = ''
   password: string = ''
 
@@ -25,9 +27,15 @@ export default class extends Vue {
       email: this.email,
       password: this.password,
     }
-    const res = await this.LoginRepository.post(params)
-    localStorage.setItem('access_token', res.access_token)
-    this.$router.push('/')
+
+    const res = await this.LoginRepository.post(params).catch(() => {
+      this.isError = true
+    })
+
+    if (res?.access_token) {
+      localStorage.setItem('access_token', res.access_token)
+      this.$router.push('/')
+    }
   }
 }
 </script>
